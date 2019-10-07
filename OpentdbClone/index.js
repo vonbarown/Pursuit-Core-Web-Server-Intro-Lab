ddca02f38bdcc0a4e442c0329b56adc7b3d863cb
vonbarown <vonielbrown@pursuit.org>
2019-10-06 22:49:16 -0400

    improving ui

--- OpentdbClone/index.js
+++ OpentdbClone/index.js
@@ -1,10 +1,11 @@
 let select;
 let correctOption;
-let currQues = 0;
-// let data;
+let currQues = -1;
+
 
 document.addEventListener('DOMContentLoaded', () => {
     getServerButton().addEventListener('click', loadDataFromServer)
+    // multipleChoice()
 })
 
 function getServerButton() {
@@ -15,20 +16,25 @@ async function loadDataFromServer() {
     const myURL = "http://localhost:3000/"
     const resp = await axios.get(myURL)
     // return resp.data.results
+
     displayResponseFromServer(resp.data.results)
 }
 
 function displayResponseFromServer(response) {
-    clearScreen()
     console.log(response)
     getAnswers(response)
-    let cardContainer = document.querySelector('#cardContainer')
-
+    let select = document.querySelector('select')
+    select.addEventListener('change', () => {
+        multipleChoice()
+    })
+    // question(response)
+    // multipleChoice()
 }
 
-const getAnswers = (data) => {
+const getAnswers = async (data) => {
+    clearScreen()
     iterateQuestion(data)
-    console.log(data);
+    // console.log(data);
     let answersArray = []
     correctOption = data[currQues].correct_answer
     answersArray.push(data[currQues].correct_answer)
@@ -36,33 +42,60 @@ const getAnswers = (data) => {
         answersArray.push(element)
     });
     console.log(currQues);
-    answersArray.forEach(el => {
-        createOptions(el)
+    let shuffledArray = shuffleAnswers(answersArray)
+    shuffledArray.forEach(el => {
+        createOptions(el);
     })
+    question(data)
 }
 
-const createOptions = async (el) => {
-    select = document.querySelector('select')
+const createOptions = (el) => {
+    select = document.querySelector('select');
     console.log();
-    let options = document.createElement('option')
-    options.value = el
-    options.innerText = el
-    select.appendChild(options)
+    let options = document.createElement('option');
+    options.value = el;
+    options.innerText = el;
+    select.appendChild(options);
 }
 
-const getSelectedMovie = select => select.options[select.selectedIndex]
+const getSelectedChoice = select => select.options[select.selectedIndex];
 
-const iterateQuestion = (data) => currQues === data.length - 1 ? currQues = 0 : currQues++
+const iterateQuestion = (data) => currQues === data.length - 1 ? currQues = 0 : currQues++;
+
+const shuffleAnswers = (arr) => {
+    return arr.sort(() => Math.random() - 0.5);
+}
+
+const question = (data) => {
+    let questionContainer = document.querySelector('#questionContainer')
+
+    let questionTag = document.createElement('p');
+    let category = document.createElement('p');
+    let difficulty = document.createElement('p');
+    let results = document.createElement('p');
+    results.id = 'results'
+
+    questionTag.innerText = `Question: ${data[currQues].question}`;
+    category.innerText = `Category: ${data[currQues].category}`;
+    difficulty.innerText = `Difficulty: ${data[currQues].difficulty}`;
+
+    questionContainer.append(category, questionTag, difficulty, results)
+}
 
 const multipleChoice = () => {
-
+    select = document.querySelector('select')
+    let results = document.querySelector('#results')
+    let opt = getSelectedChoice(select);
+    console.log(opt);
+    opt.value === correctOption ? results.innerText = `Congratulations you chose the correct answer: ${correctOption}` :
+        results.innerText = `You chose the incorrect answer! \nThe correct answer is: ${correctOption}`;
 }
 
 const clearScreen = () => {
-    select = document.querySelector('select')
+    select = document.querySelector('select');
     select.innerText = '';
-    let container = document.querySelector('#questionContainer')
+    let container = document.querySelector('#questionContainer');
     while (container.firstChild) {
-        container.removeChild(container.firstChild)
+        container.removeChild(container.firstChild);
     }
 }
